dc8e08318ad3b4eac0b6586400d55ea021e8715f
Dan Draper <dan@coderdan.co>
2022-05-16 17:36:40 +1000

    Removed util module

--- yow/lib/yow.ex
+++ yow/lib/yow.ex
@@ -1,11 +1,9 @@
 defmodule Yow do
-  import Yow.Util
-
   # Decrypts the target Ciphertext that has been decrypted
   # with AES-GCM and a repeated IV
   def break!(my_pt, my_ct, target) do
     my_pt
-    |> binary_xor(my_ct)
-    |> binary_xor(target)
+    |> :crypto.exor(my_ct)
+    |> :crypto.exor(target)
   end
 end
